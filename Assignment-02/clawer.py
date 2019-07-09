f723daf07cc15fec1d582001d616cffc1269b498
webberg <webbery@foxmail.com>
2019-07-09 17:30:19 +0800

    实现路径搜索功能

--- Assignment-02/clawer.py
+++ Assignment-02/clawer.py
@@ -58,10 +58,6 @@ for i in range(18):
                     station2 = searchObj.group(2)
                     distance = int(searchObj.group(4))
                     add_station(subway_name,station1,station2,distance)
-                # else:
-                #     print(data[idx].html)
-    # print(subway_name)
-    # print('---------')
 # print(stations)
 
 def BFS(start,dest):
@@ -81,7 +77,48 @@ def BFS(start,dest):
             break
         neighbors = neighbors + stations[front['name']]['neighbor']
         
+# BFS('西单','呼家楼')
 
-# def search(start,dest):
-#     print('')
-BFS('西单','呼家楼')
+def search(start,dest,map,sort_candidate):
+    results = []
+    pathes = [{'path':[start],'distance':0,'subways':[]}]
+
+    visited = []
+    while pathes:
+        path = pathes.pop(0)
+        station_name = path['path'][-1]
+        if station_name in visited: continue
+        visited.append(station_name)
+
+        neighbors = map[station_name]['neighbor']
+        for neighbor in neighbors:
+            new_path = path['path'] + [neighbor['name']]
+            subway = list(set(map[station_name]['subway'])&set(map[neighbor['name']]['subway']))
+            new_subway =path['subways'] + (subway if subway[0] not in path['subways'] else [])
+            path_info = {
+                'path':new_path,
+                'distance': path['distance']+neighbor['distance'],
+                'subways': new_subway
+            }
+            pathes.append(path_info)
+
+            if neighbor['name']==dest:
+                results.append(path_info)
+                break
+    return sort_candidate(results)[0]
+
+def get_stations_count(obj):
+    # print(obj)
+    return sorted(obj,key=lambda info: len(info['path']))
+
+def get_distance(obj):
+    return sorted(obj,key=lambda info: info['distance'])
+
+def get_transfer_count(obj):
+    return sorted(obj,key = lambda info: len(info['subways']))
+
+result = search('古城','望京',stations,get_stations_count)
+print(result)
+
+result = search('古城','望京',stations,get_transfer_count)
+print(result)
